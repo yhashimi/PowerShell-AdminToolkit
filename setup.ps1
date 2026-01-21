453af59da7c85075bbbc85c70dedd3cb04ece5a4
Yalda Hashimi <yaldahashemi02@gmail.com>
2026-01-21 13:31:37 +0100

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,40 @@
+﻿
+# setup.ps1
+# ------------------------------------------
+# Dieses Skript erstellt automatisch alle
+# benötigten Ordner für das Projekt.
+# Leere Ordner werden von Git nicht gespeichert,
+# deshalb sorgt dieses Skript dafür, dass alles
+# nach dem Klonen vorhanden ist.
+
+
+
+
+
+
+$root = "$env:USERPROFILE\Desktop\SecurityProject"
+
+$folders = @(
+    "$root\src",
+    "$root\src\script",
+    "$root\src\module",
+    "$root\src\function",
+    "$root\config",
+    "$root\reports",
+    "$root\reports\exports",
+    "$root\reports\logs",
+    "$root\test",
+    "$root\logs",
+    "$root\Logdateien"
+	
+
+)
+
+foreach ($folder in $folders) {
+    if (-not (Test-Path $folder)) {
+        New-Item -ItemType Directory -Path $folder -Force | Out-Null
+    }
+}
+
+New-Item -Path "$root\README.md" -ItemType File -Force
+
